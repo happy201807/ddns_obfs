0e35cf013f9128c2ee5f686ca40eb8eac792921d
happy201807 <41731934+happy201807@users.noreply.github.com>
2020-12-02 15:27:31 +0800

    Update ddns.sh

--- ddns.sh
+++ ddns.sh
@@ -6,7 +6,7 @@ apt-get install curl -y
 yum update -y
 yum install curl -y
 
-curl "https://api.dynu.com/nic/update?hostname=您申請的域名&password=您Dynu帳戶的密碼"
+curl "https://api.dynu.com/nic/update?hostname=您申請的域名&password=您Dynu帳戶的IP密碼"
 
 ## 返回下面信息表示成功 ##
 good xxx.xxx.xxx.xxx (IP地址)
@@ -15,5 +15,5 @@ good xxx.xxx.xxx.xxx (IP地址)
 設定定時更新
 crontab -e
 按i进入编辑模式，貼上下方命令，並按esc一下，再输入:wq保存
-*/1 * * * * curl "https://api.dynu.com/nic/update?hostname=您申請的域名&password=您Dynu帳戶的密碼"
+*/1 * * * * curl "https://api.dynu.com/nic/update?hostname=您申請的域名&password=您Dynu帳戶的IP密碼"
  > /dev/null 2>&1
