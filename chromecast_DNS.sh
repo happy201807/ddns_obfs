066b491811310e558b1c778345439ad45abdb69e
happy201807 <41731934+happy201807@users.noreply.github.com>
2020-12-29 21:32:09 +0800

    Update chromecast_DNS.sh

--- chromecast_DNS.sh
+++ chromecast_DNS.sh
@@ -9,11 +9,7 @@ vi /etc/firewall.user
 
 iptables -t nat -A PREROUTING -p udp --dport 53 -j REDIRECT --to-ports 53
 iptables -t nat -A PREROUTING -p tcp --dport 53 -j REDIRECT --to-ports 53
-1
-2
-iptables -t nat -A PREROUTING -s 192.168.1.1/24 -p udp --dport 53 -j DNAT --to 192.168.1.1
-iptables -t nat -A PREROUTING -s 192.168.1.1/24 -p tcp --dport 53 -j DNAT --to 192.168.1.1
-192.168.1.1是路由器IP，亲们根据自己的路由器改正。
+
 
 
 #padavan
