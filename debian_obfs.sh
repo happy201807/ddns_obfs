9615a48dba981bc5812f932e6fd7526bad82f2af
happy201807 <41731934+happy201807@users.noreply.github.com>
2021-01-03 21:26:54 +0800

    Update debian_obfs.sh

--- debian_obfs.sh
+++ debian_obfs.sh
@@ -3,10 +3,41 @@ sudo apt-get install --no-install-recommends build-essential autoconf libtool li
 # redhat 等其他系统请参照 github 官方 README
 
 git clone https://github.com/shadowsocks/simple-obfs --depth=1
-## 这里多了几笔你可能没看到，注意去掉
+
 
 cd simple-obfs
 git submodule update --init --recursive
 ./autogen.sh
 ./configure && make
 sudo make install
+
+#编译shadowsocks-libev
+apt-get update && apt-get upgrade
+
+apt-get install --no-install-recommends \
+    gettext \
+    build-essential \
+    autoconf \
+    libtool \
+    libpcre3-dev \
+    asciidoc \
+    xmlto \
+    libmbedtls-dev \
+    libev-dev \
+    libudns-dev \
+    libc-ares-dev
+
+ git clone https://github.com/jedisct1/libsodium.git
+cd libsodium
+./autogen.sh
+./configure
+make
+make install
+
+git clone https://github.com/shadowsocks/shadowsocks-libev.git
+cd shadowsocks-libev
+git submodule update --init --recursive
+./autogen.sh
+./configure
+make
+make install
