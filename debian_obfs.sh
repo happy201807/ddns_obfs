97438c288c6960b8d6fc4ca6e549dcbce5712f87
happy201807 <41731934+happy201807@users.noreply.github.com>
2021-01-03 21:10:59 +0800

    Update debian_obfs.sh

--- debian_obfs.sh
+++ debian_obfs.sh
@@ -2,7 +2,7 @@
 sudo apt-get install --no-install-recommends build-essential autoconf libtool libssl-dev libpcre3-dev libev-dev asciidoc xmlto automake
 # redhat 等其他系统请参照 github 官方 README
 
-git clone https://github.com/shado--wsocks/simple-obfs --depth=1
+git clone https://github.com/shadowsocks/simple-obfs --depth=1
 ## 这里多了几笔你可能没看到，注意去掉
 
 cd simple-obfs
