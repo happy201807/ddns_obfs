b416b61c31721d35f11ebb379b2771863b25dd01
happy201807 <41731934+happy201807@users.noreply.github.com>
2021-01-11 20:32:07 +0800

    Update acme.sh

--- acme.sh
+++ acme.sh
@@ -10,7 +10,7 @@ cd acme.sh
 利用dns手动模式 申请ssl证书
 
 1.First step:
-acme.sh --issue -d example.com --dns \
+bash acme.sh --issue -d example.com --dns \
  --yes-I-know-dns-manual-mode-enough-go-ahead-please
  
 2.Please add the TXT record to your DNS records. This step is required every time you renew your certificate. 
@@ -18,7 +18,7 @@ acme.sh --issue -d example.com --dns \
 
 3.Now retry with command.--renew
 
-acme.sh --renew -d example.com \
+bash acme.sh --renew -d example.com \
   --yes-I-know-dns-manual-mode-enough-go-ahead-please
 
-假设域名为www.domain.com,打开电脑“开始”——“运行”，输入cmd后按回车键，输入：nslookup -type=txt www.domain.com 223.5.5.5
+假设域名为www.domain.com,打开电脑“开始”——“运行”，输入cmd后按回车键，输入：nslookup -type=txt www.domain.com 8.8.8.8
