39f2e6ae82728fecdc3352ebe838838ae0411f7a
happy201807 <41731934+happy201807@users.noreply.github.com>
2021-01-18 17:31:00 +0800

    Update and rename debian_obfs.sh to simple_obfs.sh

--- debian_obfs.sh
+++
@@ -1,9 +0,0 @@
-sudo apt-get install --no-install-recommends build-essential autoconf libtool libssl-dev libpcre3-dev libev-dev asciidoc xmlto automake
-
-git clone https://github.com/shadowsocks/simple-obfs --depth=1
-
-cd simple-obfs
-git submodule update --init --recursive
-./autogen.sh
-./configure && make
-sudo make install
---
+++ simple_obfs.sh
@@ -0,0 +1,15 @@
+# Debian / Ubuntu
+sudo apt-get install --no-install-recommends build-essential autoconf libtool libssl-dev libpcre3-dev libev-dev asciidoc xmlto automake
+# CentOS / Fedora / RHEL
+sudo yum install gcc autoconf libtool automake make zlib-devel openssl-devel asciidoc xmlto libev-devel
+# Arch
+sudo pacman -Syu gcc autoconf libtool automake make zlib openssl asciidoc xmlto
+# Alpine
+apk add gcc autoconf make libtool automake zlib-dev openssl asciidoc xmlto libpcre32 libev-dev g++ linux-headers
+
+git clone https://github.com/shadowsocks/simple-obfs.git
+cd simple-obfs
+git submodule update --init --recursive
+./autogen.sh
+./configure && make
+sudo make install
