fddb3b1b81d848ce94b085d200c35076b862578f
happy201807 <41731934+happy201807@users.noreply.github.com>
2021-03-04 19:34:20 +0800

    Create Rebuild_R4S.sh

---
+++ Rebuild_R4S.sh
@@ -0,0 +1,19 @@
+# CONFIG_PACKAGE_adbyby=y
+# CONFIG_PACKAGE_frpc=y
+# CONFIG_PACKAGE_frps=y
+# CONFIG_PACKAGE_gost=y
+# CONFIG_PACKAGE_kmod-wireguard=y
+
+# CONFIG_PACKAGE_luci-app-adbyby-plus=y
+# CONFIG_PACKAGE_luci-app-frpc=y
+# CONFIG_PACKAGE_luci-app-frps=y
+# CONFIG_PACKAGE_luci-app-gost=y
+# CONFIG_PACKAGE_luci-app-wireguard=y
+# CONFIG_PACKAGE_luci-app-zerotier=y
+
+# CONFIG_PACKAGE_luci-proto-wireguard=y
+# CONFIG_PACKAGE_wireguard-tools=y
+# CONFIG_PACKAGE_zerotier=y
+
+
+CONFIG_PACKAGE_igmpproxy=y
